e608b21bb1f4f92fb0faed4b3911c0847a9fc5b4
ishanz0 <ishankataria0422@gmail.com>
2023-01-04 16:19:53 +0530

    Chapter-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading1 = React.createElement("h1", {id: "title", key: "1"}, "Namaste EveryOne from React!");
+        const heading2 = React.createElement("h1", {id: "title", key: "2"}, "Namaste EveryOne from React twice!");
+
+        const container = React.createElement("div", {id: "container"}, [heading1, heading2]);
+
+        const root1 = ReactDOM.createRoot(document.getElementById("root"));
+        console.log(heading1);  
+        // passing react element inside div root
+        root1.render(container);
