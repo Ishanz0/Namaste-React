a3f6b8ac024135dff897de9cfed8944e09f87294
ishanz0 <ishankataria0422@gmail.com>
2023-01-11 09:53:25 +0530

    Chapter - 3 optimize

--- App.js
+++ App.js
@@ -1,34 +1,21 @@
 import React from "react"; 
 import ReactDOM from "react-dom/client";
 
-//React
-const heading1 = React.createElement(
-        "h1", 
-        {id: "title", key: "1"}, 
-        "Namaste EveryOne from React!");
+const Title = () => (
+        <h1 id="title" key="1">Namaste React!</h1>
+);
 
-const heading2 = <h1 id="title" key="2">Namaste EveryOne from JSX!</h1>; // JSX
-
-const container = React.createElement(
-        "div", 
-        {id: "container"}, 
-        [heading1, heading2]);        
-
-const heading3 = () => {
+// Composing Components
+const HeaderComponent = () => {
         return (
         <div>
-                {/* for using element inside func. component */}
-                {heading2}
-                {/* use {<heading2 />} or {heading2()} for using func. component inside another */}
-                <h2 id="title" key="3">Namaste EveryOne from functional component!</h2>
+                <Title />
+                <h2>Namaste EveryOne from functional component!</h2>
                 <h2> This is h2 tag! </h2>
         </div>
         );
 };
 
-
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
-// passing react element inside div root
-// root1.render(container);
-root1.render(heading3());  
-// you can also use <heading3 /> inside render     
+
+root1.render(<HeaderComponent />);     
