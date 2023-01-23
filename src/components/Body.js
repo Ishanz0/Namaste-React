37a118dfbddfcca53583245c1db5436eb5647773
ishanz0 <ishankataria0422@gmail.com>
2023-01-23 15:10:48 +0530

    Chapter - 06

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,13 +4,14 @@ import { restaurantList } from "../constants";
 import Shimmer from "./Shimmer";
 
 function filterData(searchText, restaurants) {
-    return restaurants.filter((restaurant) => restaurant.data.name.includes(searchText));
+    return restaurants.filter((restaurant) => restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase()));
 }
 
 const Body = () => { 
 
     const [searchText, setSearchText] = useState("");  // to create state variable
-    const [restaurants, setRestaurants] = useState([]);
+    const [allRestaurants, setAllRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
     // empty dependency array => once after rendering
     // dependency array [searchText] => once after rendering + everytime after rerendering (my searchtext changes)
@@ -23,26 +24,38 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0384171&lng=72.5288016&page_type=DESKTOP_WEB_LISTING")
         const json = await data.json();
         console.log(json);
-        setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
     }
 
-    return (restaurants.length === 0 ) ? <div className="shimmer-card"><Shimmer /></div> : (
+    if (!allRestaurants) return null;
+
+    // if (filteredRestaurants.length === 0) return <h1> No Restaurants Found! </h1>
+
+    return (allRestaurants.length === 0 ) ? <div className="shimmer-card"><Shimmer /></div> : (
     <>
     <div className="search-container">
         <input type="text" placeholder="Search Your Bite!" className="search-input" value={searchText}
         onChange={(e) => {setSearchText(e.target.value); }} />
         <button className="search-btn" onClick={
             () => {
-                const data = filterData(searchText, restaurants);
-                setRestaurants(data);
+                const data = filterData(searchText, allRestaurants);
+                setFilteredRestaurants(data);
+                console.log(data.length)
             }
         }>Search</button>
     </div>
 
     <div className="restaurant-list">
-        
-        {restaurants?.map((restaurant) => {
-            return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>;
+
+        {filteredRestaurants?.map((restaurant) => {
+            return (
+                (filteredRestaurants.length > 0) ? (
+                    <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>
+                ) : (
+                    <Shimmer />
+                )
+            )
         })}
     </div>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,10 @@
+import { useState } from "react";
+
+// const loggedInUser = () => {
+//     // API call for authentication
+//     return true;
+// }
+
 const Title = () => (
     <a href="/">
     <img src="https://images-platform.99static.com/nMPDdzn3g5qcYI75Cm9U32Rt83s=/500x500/top/smart/99designs-contests-attachments/19/19919/attachment_19919182"
@@ -6,6 +13,8 @@ const Title = () => (
 );
 
 const Header = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     
     return (
 
@@ -20,6 +29,11 @@ const Header = () => {
                 <li>Cart</li>
             </ul>
         </div>
+        {isLoggedIn ? (
+            <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+        ) : (
+            <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        )}
     </div>
     );
 };
